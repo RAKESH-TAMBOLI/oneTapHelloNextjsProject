83ccb6ab478aa3b412214fbd61ce08f31e15c352
Rakesh Tamboli <rakeshtamboli006688@gmail.com>
2022-06-16 18:49:29 +0530

    make it better

---
+++ next.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    future: {
+        webpack5: true
+    },
+    webpack(config) {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false
+        }
+
+        return config
+    }
+}
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
